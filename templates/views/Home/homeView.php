a03961e4a88627de2ee3a2f2d6a087be00bd1012
JuliusMatadamas <julius3877@gmail.com>
2021-12-14 15:10:33 -0600

    Se termina la session header de la pagina home.

--- templates/views/Home/homeView.php
+++ templates/views/Home/homeView.php
@@ -1,11 +1,13 @@
 <?php require INCLUDES.'header.php'; ?>
 		<header>
             <div class="container">
-                <a href="<?php echo URL; ?>">
-                    <img src="<?php echo IMAGES; ?>/logo-bg-solid.png" alt="">
-                </a>
-
                 <nav>
+                    <input type="checkbox" id="nav-toggle">
+
+                    <a href="<?php echo URL; ?>">
+                        <img src="<?php echo IMAGES; ?>/logo-bg-solid.png" alt="Dish Matamoros">
+                    </a>
+
                     <ul>
                         <li>
                             <a href="#inicio">
@@ -28,7 +30,45 @@
                             </a>
                         </li>
                     </ul>
+
+                    <label for="nav-toggle" class="icon-burger">
+                        <div class="line"></div>
+                        <div class="line"></div>
+                        <div class="line"></div>
+                    </label>
                 </nav>
             </div>
-		</header>
+        </header>
+
+        <section id="content">
+            <section id="inicio">
+                <h1>Inicio</h1>
+                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi autem, facilis fuga illum nesciunt tempora! Alias dignissimos, eaque eum eveniet hic labore laborum magni odio quae quibusdam, quo repellendus sit.</span><span>Cum dolorem doloribus earum incidunt ipsum, laboriosam, libero molestias nisi porro provident repellat sequi vel voluptas. Ab aliquid amet inventore iure maxime minus odio, placeat quaerat repellat sed voluptatibus voluptatum.</span><span>Ab blanditiis cum dignissimos eos esse excepturi ipsam molestias numquam, placeat, quis quos ut veniam. Ad commodi dicta earum esse, facere fugit harum illum magni maiores odio officiis, unde veniam?</span><span>Adipisci deserunt dolorem doloribus provident quia tempora tenetur. Accusamus aliquid, aperiam autem cum dignissimos eaque ex illum libero magnam maxime molestias nemo nulla pariatur, placeat provident, quis tempora vel voluptatibus.</span><span>Ab aliquam, amet cum distinctio dolor dolores eaque eveniet facere facilis fuga illo incidunt itaque iure libero magnam minus non odio, quaerat quibusdam recusandae repudiandae, totam ullam vitae voluptates voluptatibus.</span></p>
+            </section>
+
+            <hr>
+
+            <section id="servicios">
+                <h1>Servicios</h1>
+                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi autem, facilis fuga illum nesciunt tempora! Alias dignissimos, eaque eum eveniet hic labore laborum magni odio quae quibusdam, quo repellendus sit.</span><span>Cum dolorem doloribus earum incidunt ipsum, laboriosam, libero molestias nisi porro provident repellat sequi vel voluptas. Ab aliquid amet inventore iure maxime minus odio, placeat quaerat repellat sed voluptatibus voluptatum.</span><span>Ab blanditiis cum dignissimos eos esse excepturi ipsam molestias numquam, placeat, quis quos ut veniam. Ad commodi dicta earum esse, facere fugit harum illum magni maiores odio officiis, unde veniam?</span><span>Adipisci deserunt dolorem doloribus provident quia tempora tenetur. Accusamus aliquid, aperiam autem cum dignissimos eaque ex illum libero magnam maxime molestias nemo nulla pariatur, placeat provident, quis tempora vel voluptatibus.</span><span>Ab aliquam, amet cum distinctio dolor dolores eaque eveniet facere facilis fuga illo incidunt itaque iure libero magnam minus non odio, quaerat quibusdam recusandae repudiandae, totam ullam vitae voluptates voluptatibus.</span></p>
+            </section>
+
+            <hr>
+
+            <section id="paquetes">
+                <h1>Paquetes</h1>
+                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi autem, facilis fuga illum nesciunt tempora! Alias dignissimos, eaque eum eveniet hic labore laborum magni odio quae quibusdam, quo repellendus sit.</span><span>Cum dolorem doloribus earum incidunt ipsum, laboriosam, libero molestias nisi porro provident repellat sequi vel voluptas. Ab aliquid amet inventore iure maxime minus odio, placeat quaerat repellat sed voluptatibus voluptatum.</span><span>Ab blanditiis cum dignissimos eos esse excepturi ipsam molestias numquam, placeat, quis quos ut veniam. Ad commodi dicta earum esse, facere fugit harum illum magni maiores odio officiis, unde veniam?</span><span>Adipisci deserunt dolorem doloribus provident quia tempora tenetur. Accusamus aliquid, aperiam autem cum dignissimos eaque ex illum libero magnam maxime molestias nemo nulla pariatur, placeat provident, quis tempora vel voluptatibus.</span><span>Ab aliquam, amet cum distinctio dolor dolores eaque eveniet facere facilis fuga illo incidunt itaque iure libero magnam minus non odio, quaerat quibusdam recusandae repudiandae, totam ullam vitae voluptates voluptatibus.</span></p>
+            </section>
+
+            <hr>
+
+            <section id="contacto">
+                <h1>Contacto</h1>
+                <p><span>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi autem, facilis fuga illum nesciunt tempora! Alias dignissimos, eaque eum eveniet hic labore laborum magni odio quae quibusdam, quo repellendus sit.</span><span>Cum dolorem doloribus earum incidunt ipsum, laboriosam, libero molestias nisi porro provident repellat sequi vel voluptas. Ab aliquid amet inventore iure maxime minus odio, placeat quaerat repellat sed voluptatibus voluptatum.</span><span>Ab blanditiis cum dignissimos eos esse excepturi ipsam molestias numquam, placeat, quis quos ut veniam. Ad commodi dicta earum esse, facere fugit harum illum magni maiores odio officiis, unde veniam?</span><span>Adipisci deserunt dolorem doloribus provident quia tempora tenetur. Accusamus aliquid, aperiam autem cum dignissimos eaque ex illum libero magnam maxime molestias nemo nulla pariatur, placeat provident, quis tempora vel voluptatibus.</span><span>Ab aliquam, amet cum distinctio dolor dolores eaque eveniet facere facilis fuga illo incidunt itaque iure libero magnam minus non odio, quaerat quibusdam recusandae repudiandae, totam ullam vitae voluptates voluptatibus.</span></p>
+            </section>
+
+            <footer>
+            </footer>
+
+        </section>
 <?php require INCLUDES.'footer.php'; ?>
