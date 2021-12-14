829c1aafa7314e0724b7f9fa3087872880afb06e
JuliusMatadamas <julius3877@gmail.com>
2021-12-13 19:09:28 -0600

    Se termina la pagina de error 404 y se modifican los logos.

--- templates/views/Error/404View.php
+++ templates/views/Error/404View.php
@@ -2,16 +2,14 @@
 		<section class="container flex">
 			<div class="error__container text-center">
 				<a href="<?php echo URL; ?>">
-					<img src="<?php echo IMAGES; ?>/logo-bg-white-solid.png" alt="Dish Matamoros">
+					<img src="<?php echo IMAGES; ?>/logo-bg-white.png" alt="Dish Matamoros">
 				</a>
 
-				<h1>matamoros</h1>
+				<h1>ERROR 404</h1>
 
-				<h2>404</h2>
+				<h2>Página no encontrada</h2>
 
-				<h3>Página no encontrada</h3>
-
-				<p>La página solicitada no fue encontrada dentro del sitio web, verifique la url ingresada o de clic en el botón siguiente para regresar a la página anterior.</p>
+				<p>Verifique la url o de clic en el botón siguiente para regresar a la página anterior.</p>
 
 				<button type="button" class="btn btn-outline-danger" onclick="historyReturn()">Regresar</button>
 			</div>
